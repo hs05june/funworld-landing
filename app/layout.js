7894f96e6f6fe97fd8bf702ed48dd82530c6463c
hs05june <harpreet.singh.cse21@itbhu.ac.in>
2024-01-22 23:24:48 +0530

    Added Analytics
    Signed-off-by: hs05june <harpreet.singh.cse21@itbhu.ac.in>

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Repeating/Navbar";
 import Script from "next/script";
-
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,31 +13,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // useEffect(() => {
-  //   Modal.setAppElement("#__next");
-  // }, []);
-
-  // useEffect(() => {
-  //   // Your Google Tag Manager script here
-  //   (function (w, d, s, l, i) {
-  //     w[l] = w[l] || [];
-  //     w[l].push({
-  //       "gtm.start": new Date().getTime(),
-  //       event: "gtm.js",
-  //     });
-  //     var f = d.getElementsByTagName(s)[0],
-  //       j = d.createElement(s),
-  //       dl = l != "dataLayer" ? "&l=" + l : "";
-  //     j.async = true;
-  //     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-  //     f.parentNode.insertBefore(j, f);
-  //   })(window, document, "script", "dataLayer", "GTM-MTC3R6J3");
-  // }, []); // Empty dependency array to ensure it runs only on the client side
-
   return (
     <>
       <Script src="https://checkout.razorpay.com/v1/checkout.js" />
       <html lang="en">
+        <GoogleAnalytics GA_TRACKING_ID={'GTM-MTC3R6J3'} />
         <title>Funworld </title>
         {/* {typeof window !== "undefined" && (
           <AnimatedCursor
