146683c05f17b4d74a160be2dbedc410cb7c8926
Anurag <anurag.kamboj.mat21@itbhu.ac.in>
2024-02-05 01:34:59 +0530

    anurag

--- components/Rides/Rides.jsx
+++ components/Rides/Rides.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 const Rides = ({ img, text, index }) => {
-
   return (
-    <div className='w-full flex flex-col imgcontainer'>
-      {/* <Link href={`/ridedetails/?ride=${index}`} > */}
-        <img priority src={img} className="image h-[250px]" alt="abc" />
-        <div className='w-full font-spartan py-4 text-left text-4xl font-bold max-xl:text-2xl'>
-          <div className=' trans   '>
-            <span className='mr-0 opacity-0 whitespace-nowrap spannum ml-6'></span><span>{text}</span>
-          </div>
+    <Link
+      img={img}
+      href={`/ridedetails/${index}`}
+      className="w-full flex flex-col imgcontainer cursor-pointer"
+    >
+      <img priority src={img} className="image h-[250px]" alt="abc" />
+      <div className="w-full font-spartan py-4 text-left text-4xl font-bold max-xl:text-2xl">
+        <div className=" trans   ">
+          <span className="mr-0 opacity-0 whitespace-nowrap spannum ml-6"></span>
+          <span>{text}</span>
         </div>
-      {/* </Link> */}
-    </div>
-  )
-}
+      </div>
+    </Link>
+  );
+};
 
-export default Rides
+export default Rides;
