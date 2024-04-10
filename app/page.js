e7f27fc024d6e44067d3d8f265d74f753d03272a
Naveen R <naveenr@naveenr.com>
2024-04-10 16:40:02 +0530

    Feat: [Issue-01]: Addeed popup for mandaroty clothing

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+import { useEffect } from "react";
 
 import Hero from "@/components/Home/Hero";
 import ParkAttractions from "@/components/Home/ParkAttractions";
@@ -6,8 +7,30 @@ import ParkAttractions from "@/components/Home/ParkAttractions";
 import Welcome from "@/components/Home/Welcome";
 import YourTicket from "@/components/Home/YourTicket";
 
+import Swal from "sweetalert2";
+
 export default function Home() {
-  
+  useEffect(() => {
+    if (window && window !== undefined) {
+      const acceptedTerms = sessionStorage.getItem("acceptedTerms");
+      if (!acceptedTerms) {
+        Swal.fire({
+          title: "Please Note!",
+          html: "<p>Nylon swim clothes are <b>mandtory</b><br />to enter Water World.</p>",
+          icon: "info",
+          showCancelButton: false,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "OK!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            sessionStorage.setItem("acceptedTerms", true);
+          }
+        });
+      }
+    }
+  }, []);
+
   return (
     <main className="w-full">
       <Hero />
@@ -15,6 +38,5 @@ export default function Home() {
       <YourTicket />
       <ParkAttractions />
     </main>
-  )
+  );
 }
-
