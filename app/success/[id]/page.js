43c40327ed8ff1ca3841ed200c42ffcfef674349
Anurag <anurag.kamboj.mat21@itbhu.ac.in>
2024-04-21 12:47:36 +0530

    anurag

--- app/success/[id]/page.js
+++ app/success/[id]/page.js
@@ -7,9 +7,7 @@ const Success = ({ params }) => {
   const [ticket, setTicket] = useState({});
   useEffect(() => {
     axios
-      .get(
-        `https://funworld-backend.vercel.app/api/soldtickets?id=${params.id}`
-      )
+      .get(`https://free.funworldbackend.tech//api/soldtickets?id=${params.id}`)
       .then((res) => {
         setTicket(res.data.message);
         // console.log(res.data.message);
